845501cbddd113d7ed96b17005018a777905941d
devdomsos <devdomsos@gmail.com>
2023-06-12 20:06:28 +0200

    Feature(Prismat-Mktplc-Setup): Correct Twitter links

--- components/Head.tsx
+++ components/Head.tsx
@@ -31,7 +31,7 @@ export const Head: FC<Props> = ({
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@Prismat" />
+      <meta name="twitter:site" content="@Prismat_io" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={ogImage} />
--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -100,7 +100,7 @@ export const Footer = () => {
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://twitter.com/Prismat.io"
+            href="https://twitter.com/Prismat_io"
           >
             <Button size="xs" color="gray3">
               <FontAwesomeIcon icon={faTwitter} width={14} height={14} />
